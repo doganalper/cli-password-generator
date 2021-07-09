e1c779c3280e1e56f7b6daecf94926ecfd0728c6
Alper Doğan <48688801+doganalper@users.noreply.github.com>
2021-07-09 12:35:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const program = require("commander");
+const chalk = require("chalk");
+const clipboard = require("clipboardy");
+const log = console.log;
+const createPassword = require("./utils/createPassword");
+const savePassword = require("./utils/savePassword");
+
+program.version("1.0.0").description("CLI for generating password");
+
+program
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
+
+// Generate Password
+const generatedPassword = createPassword(length, numbers, symbols);
+
+// Save password to file on demand
+if (save) {
+  savePassword(generatedPassword);
+}
+
+// Copy to clipboard
+clipboard.writeSync(generatedPassword);
+
+log(chalk.red("Generated Password: ") + chalk.bold(generatedPassword));
+log(chalk.yellow("Password copied to clipboard"));
